ab80d0d6775d0756b0581cd195e505d49fc1c0e8
Roman Kotov <RomanKotov@users.noreply.github.com>
2021-10-27 12:04:33 +0300

    Workaround for comments dark mode

--- docs/.vuepress/theme/components/Comments.vue
+++ docs/.vuepress/theme/components/Comments.vue
@@ -7,7 +7,7 @@
       categoryId="DIC_kwDOD46OyM4B_mWS"
       mapping="og:title"
       reactionsEnabled="1"
-      theme="preferred_color_scheme"
+      :theme="theme"
       lang="en"
       emitMetadata="0"
     />
@@ -19,9 +19,12 @@ import { Giscus } from '@giscus/vue';
 import { usePageFrontmatter } from "@vuepress/client";
 import { computed } from "vue";
 import type { DefaultThemePageFrontmatter } from "@vuepress/theme-default/shared";
+import { useDarkMode } from "@vuepress/theme-default/lib/client";
 
+const isDarkMode = useDarkMode();
 const frontmatter = usePageFrontmatter<DefaultThemePageFrontmatter>();
 const shouldShowComments = computed(() => frontmatter.value.comments === true);
+const theme = computed(() => isDarkMode.value ? "dark" : "preferred_color_scheme");
 </script>
 
 <style lang="scss">
